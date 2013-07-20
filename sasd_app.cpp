852e325c05c01ae863b0d8987393c21abdcc7484
anamupota <null>
2013-07-20 02:21:07 -0700

    abaflkdzjdfsssdfqs

---
+++ sasd_app.cpp
@@ -0,0 +1,47 @@
+/*
+
+edited!!!
+
+lsddfjklsjqfksdklfjqlksfjlkqsdjfqdsf
+qdsflkqsfjlkqsflkqjdsf
+
+
+qmlfksdqmfljdskjmjlfhmqlkjqlkjd fqlk  jlk jklqjfmlqkdsjfqlkmsdjf qsdùlkfqjfmjqsd fmklsqdjf kklqmjfqjkshfqkjsfhkjmqs dhjk jk hqjksh qjhf hqdsh j*
+
+j
+hfqjksfhqfjkqlfklqf
+
+
+
+* Simple Wiselqlk another mqsldkf Wild edit from GitHub  nonendsqf fq fq fqf qfqfq
+*/
+
+#include "external_interface/external_interface.h"
+#include "algorithms/routing/tree/tree_routing.h"
+
+typedef wiselib::OSMODEL Os;
+
+class ExampleApplication
+{
+    public:
+        void init( Os::AppMainParameter& value )
+    
+    private:
+        Os::Radio::self_pointer_t radio_;
+        Os::Timer::self_pointer_t timer_;
+        Os::Debug::self_pointer_t debug_;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// --------------------------------------------------------------------------
+wiselib::WiselibApplication example_app;
+// --------------------------------------------------------------------------
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+//  another
+/////////////////////////////////////////////////////////////////////////////
+void application_main( Os::AppMainParameter& value )
+{
+    example_app.init( value );
+}
